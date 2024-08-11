bfeb648a1e040bcfc1c053b916d9b293778d3648
mxrsel <marsel14082006@gmail.com>
2024-08-11 14:41:45 +0600

    added cors options to index

--- chat-api/index.ts
+++ chat-api/index.ts
@@ -1,7 +1,22 @@
 import express from 'express'
+import messagesRouter from "./routers/messages";
+import cors, {CorsOptions} from "cors";
 
 const app = express();
 const port = 8000;
+
+const whiteList = ['http://localhost:5173'];
+const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+        if(!origin || whiteList.indexOf(origin) !== -1){
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use('/messages', messagesRouter);
 
